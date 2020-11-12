0812ad25d49af58525e11cc1263ef6ba240d8cb1
Romulo Sobreira <romurevoltado@gmail.com>
2020-11-12 08:06:41 -0300

    erro HttpResponse não instanciado

--- core/views.py
+++ core/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import render, redirect
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+
+
 # Create your views here.
 
 # def index(request): metodo para colocar essa pagina como home do site
 #     return redirect('/agenda/')
 
+def login_user(request):
+    return render(request, 'login.html')
+
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(usermane=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+    else:
+        redirect('/')
+
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
     usuario = request.user
     evento = Evento.objects.filter(usuario=usuario)
     dados = {'eventos': evento}
-    return  render(request, 'agenda.html', dados)
+    return render(request, 'agenda.html', dados)
